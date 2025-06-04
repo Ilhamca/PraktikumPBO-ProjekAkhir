57202bf15114d5aaa415e576d7f00bd411c11a3a
Ilhamca <stoneruiner@gmail.com>
2025-06-04 16:14:55 +0700

    Added Models

---
+++ src/Model/Users/ModelUsers.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Users;
+
+/**
+ *
+ * @author Iam
+ */
+public class ModelUsers {
+
+    private int id;
+    private String username, password;
+    private Role role;
+
+    public enum Role {
+        ADMIN,
+        STAFF
+    }
+
+    // Default Constructor - Creates all fields unset (null or 0)
+    public ModelUsers() {
+    }
+
+    // Parameterized Constructor - Instantly initialize object in one line
+    public ModelUsers(int id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
